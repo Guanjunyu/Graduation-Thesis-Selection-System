6f7232bd2e19e778acd8ca99ddede2b13f506705
GuanJunYu <junyuguan63@gmail.com>
2024-12-27 14:45:10 +0800

    [GJY]测试修改

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
 # Graduation Thesis Selection System
 
-
-
-
-
-
-
-
-
 ThinkPHP6运行环境要求PHP7.2+，兼容PHP8.1
 
-[官方应用服务市场](https://market.topthink.com) | [`ThinkAPI`——官方统一API服务](https://docs.topthink.com/think-api)
-
-ThinkPHPV6.0版本由[亿速云](https://www.yisu.com/)独家赞助发布。
 
 ## 主要新特性
 
--- config/captcha.php
+++ config/captcha.php
@@ -7,7 +7,7 @@ return [
     //验证码位数
     'length'   => 5,
     // 验证码字符集合
-    'codeSet'  => '2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
+    'codeSet'  => '12345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',
     // 验证码过期时间
     'expire'   => 1800,
     // 是否使用中文验证码
